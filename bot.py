0cc8b641253c37511f1f9a9eeb21ae6c153f646f
Михаил Никифоров <Mihanik1608@hotmail.com>
2022-06-11 00:46:51 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,29 @@
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram_dialog import DialogRegistry
+from config import API_TOKEN
+
+
+class MyBot:
+    storage = MemoryStorage()
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher(bot, storage=storage)
+    registry = DialogRegistry(dp)
+
+    @classmethod
+    async def run_bot(cls):
+        logging.basicConfig(level=logging.INFO)
+        await cls.dp.start_polling()
+
+    @classmethod
+    def register_handler(cls, **kwargs):
+        try:
+            cls.dp.register_message_handler(kwargs["method"], commands=kwargs["commands"])
+        except:
+            cls.dp.register_message_handler(kwargs["method"], text=kwargs["text"])
+
+    @classmethod
+    def register_dialogs(cls, *args):
+        for dialog in args:
+            cls.registry.register(dialog)
