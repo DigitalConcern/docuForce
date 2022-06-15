59ef7e1af0ebf3e3170a21e23237069255d9e07d
den10b <den10b@yandex.ru>
2022-06-15 18:41:38 +0300

    Траблы с либой(((

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import base64
 import logging
-from typing import IO, Union
+from typing import IO, Union, Optional
 
 import requests
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import Message
 from aiogram_dialog import DialogRegistry
-from aiogram_dialog.manager.protocols import MediaAttachment
+from aiogram_dialog.manager.protocols import MediaAttachment, MediaId
 from aiogram_dialog.message_manager import MessageManager
 
 from config import API_TOKEN
@@ -20,6 +21,21 @@ class SuperMesssageManager(MessageManager):
             return base64.b64decode(response.text)
         else:
             return open(media.path, "rb")
+
+    # async def get_media_id(message: Message) -> Optional[MediaId]:
+    #     media = (
+    #             message.audio or
+    #             message.animation or
+    #             message.document or
+    #             (message.photo[-1] if message.photo else None) or
+    #             message.video
+    #     )
+    #     if not media:
+    #         return None
+    #     return MediaId(
+    #         file_id=media.file_id,
+    #         file_unique_id=media.file_unique_id,
+    #     )
 
 class MyBot:
     storage = MemoryStorage()
