a57c9b0e05293ffa37e2246692fc99b285da0a0f
den10b <den10b@yandex.ru>
2022-06-26 01:01:09 +0300

    push

--- source/web/views.py
+++ source/web/views.py
@@ -10,5 +10,12 @@ from . import forms
 from django.utils.translation import gettext as _
 
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+class UsersPageView(TemplateView):
+    model = models.ActiveUsers
+    template_name = 'users.html'
+
+    # fields = ['user_id', 'code_name']
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = models.ActiveUsers.objects.all()
+        return context
